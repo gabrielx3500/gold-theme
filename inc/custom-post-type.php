f576ab9b979c4cbee3b31bde1fc31ccbd3501fe2
gabrielx3500 <gabrielx3500@gmail.com>
2017-03-19 18:07:50 -0430

    Lesson 11

--- inc/custom-post-type.php
+++ inc/custom-post-type.php
@@ -12,6 +12,8 @@
 $contact = get_option( 'activate_contact' );
 if( @$contact == 1 ){
 	add_action( 'init', 'sunset_contact_custom_post_type' );
+	add_filter('manage_sunset-contact_posts_columns','sunset_set_contact_columns');
+	add_action('manage_sunset-contact_posts_custom_column','sunset_contact_custom_column',10,2);
 }
 
 /* CONTACT CPT */
@@ -36,4 +38,25 @@ function sunset_contact_custom_post_type() {
 	
 	register_post_type( 'sunset-contact', $args );
 	
+}
+
+function sunset_set_contact_columns( $columns ){
+	$newColumns = array();
+	$newColumns['title'] = 'Full Name';
+	$newColumns['message'] = 'Message';
+	$newColumns['email'] = 'Email';
+	$newColumns['date'] = 'Date';
+	return $newColumns;
+}
+
+function sunset_contact_custom_column(){
+   switch($column){
+   	case 'message':
+   	  echo get_the_excerpt();
+   	  break;
+   	case 'email':
+   	  echo 'email address';
+   	  break;  
+
+   }
 }
